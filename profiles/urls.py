7ac8cef31268a3376af62b4290d3be4d67e7f80d
Stanislav Smolevich <smolestan@Stanislavs-MacBook-Pro.local>
2019-07-28 18:45:35 +0300

    Added route for profile update

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from .api import ProfileViewSet
 
 router = routers.DefaultRouter()
 router.register('', ProfileViewSet, 'profile')
+router.register('<pk>', ProfileViewSet, 'update')
 
 urlpatterns = router.urls
